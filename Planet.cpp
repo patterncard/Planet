5f964f127f127f6c7a07c5110e3750f34934f8d5
Patrycja <patrycja@pop-os.localdomain>
2019-05-03 13:10:22 +0200

    init

---
+++ Planet.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string.h>
+
+class Planet {
+private:
+	char name[20];
+	int age;
+public:
+	int foodAmount;
+
+	Planet(char name[20],int age)
+	{
+		this->age = age;
+		strcpy(this->name, name);
+		this->foodAmount= rand() % 300 + 15;
+	}
+
+};
+
+
+
+int main()
+{
+	//std::cout << foodAmount << std::endl;
+	return 0;
+}
