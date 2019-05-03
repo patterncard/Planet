64ff7a3b801fcef490744aadb0e7231b35fb29ca
patterncard <patcad@o2.pl>
2019-05-03 23:19:08 +0200

    task 4

--- Planet.cpp
+++ Planet.cpp
@@ -1,26 +1,25 @@
 #include <iostream>
 #include <string.h>
 
-class Planet {
+class Planet
+{
 private:
 	char name[20];
 	int age;
+
 public:
 	int foodAmount;
 
-	Planet(char name[20],int age)
+	Planet(char name[20], int age)
 	{
 		this->age = age;
 		strcpy(this->name, name);
-		this->foodAmount= rand() % 300 + 15;
+		this->foodAmount = rand() % 300 + 15;
 	}
-
 };
-
-
 
 int main()
 {
-	std::cout << "foodAmounttt" << std::endl;
+	//std::cout << foodAmount << std::endl;
 	return 0;
 }
